2d6b4e47b660c9873dbaf00306178a131ab2d8de
tammy le <tammyle@tammys-MBP.attlocal.net>
2017-10-02 14:15:34 -0700

    functions done

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beer Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style Burger', true);
