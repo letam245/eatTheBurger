d23bce84e36534ba6bdd45ed1bdf98d153f61ff0
tammy le <tammyle@dhcp-v123-197.mobile.uci.edu>
2017-10-02 19:15:23 -0700

    heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,32 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "chibalun83",
-    database: "burgers_db"
-});
+var connection;
+
+if(process.env.CLEARDB_DATABASE_URL){
+    connection = mysql.createConnection({
+        host: process.env.HOST,
+        user: process.env.USERNAME,
+        password: process.env.PASSWORD,
+        database: process.env.DATABASE_NAME
+    })
+} else{
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "chibalun83",
+        database: "burgers_db"
+    });
+};
+
+// var connection = mysql.createConnection({
+//     port: 3306,
+//     host: "localhost",
+//     user: "root",
+//     password: "chibalun83",
+//     database: "burgers_db"
+// });
 
 connection.connect(function (err) {
     if (err) {
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 7000;
 
 var app = express();
+app.set("port", process.env.PORT || 7000);
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
@@ -25,6 +25,6 @@ var routes = require("./controllers/burger_controller.js");
 
 app.use("/", routes);
 
-app.listen(port, function(){
-    console.log("app is listening to: " +  port)
+app.listen(app.get("port"), function(){
+    console.log("app is listening to: " +  app.get("port"));
 });
